516b1e75c658b3d644b1b5b4685a04f8151df534
fababu <fababu13@gmail.com>
2014-03-11 21:34:20 +0100

    Add delete function to wishes.

--- app/scripts/controllers/wishes.js
+++ app/scripts/controllers/wishes.js
@@ -5,8 +5,14 @@ angular.module('wihaApp')
     $scope.wishes = Wish.query();
     $scope.makeAWish = function makeAWish() {
       var wish = new Wish({text: $scope.newWish});
-      wish.$save(function success(value) {
+      wish.$save(function success() {
         $scope.newWish = '';
+        $scope.wishes = Wish.query();
+      });
+    };
+    $scope.deleteAWish = function deleteAWish(wishId) {
+      var wish = new Wish({id: wishId});
+      wish.$delete(function success() {
         $scope.wishes = Wish.query();
       });
     };
